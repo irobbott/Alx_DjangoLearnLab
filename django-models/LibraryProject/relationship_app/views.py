6707df44c7bd1bdeea4a8205e2aabe6ee0046831
Iconic Robbott <dev.robbott@gmail.com>
2025-02-21 22:11:08 -0800

    Implementing User Authentication in Django

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,16 +1,29 @@
-from django.shortcuts import render
-from django.views.generic import DetailView
-from django.views.generic.detail import DetailView
-from .models import Book
-from .models import Library
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.shortcuts import render, redirect
 
-# Function-based view to list all books
-def list_books(request):
-    books = Book.objects.all()
-    return render(request, 'relationship_app/list_books.html', {'books': books})
+def user_login(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('list_books')  # Redirect to a relevant page
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
 
-# Class-based view to show library details
-class LibraryDetailView(DetailView):
-    model = Library
-    template_name = 'relationship_app/library_detail.html'
-    context_object_name = 'library'
+def user_logout(request):
+    logout(request)
+    return render(request, 'logout.html')
+
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('list_books')
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
