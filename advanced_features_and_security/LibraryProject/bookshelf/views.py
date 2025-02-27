fd78338d4994bd88f2a406239bce83c092eb2608
Iconic Robbott <dev.robbott@gmail.com>
2025-02-27 17:04:50 +0100

    Implementing Security Best Practices in Django

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -43,3 +43,7 @@ def book_search(request):
     query = request.GET.get('q', '')
     books = Book.objects.filter(Q(title__icontains=query) | Q(author__icontains=query))
     return render(request, 'bookshelf/book_list.html', {'books': books, 'query': query})
+
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'bookshelf/book_list.html', {'books': books})
