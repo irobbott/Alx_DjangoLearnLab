ac69a395f48a21dca23e72b18e47cfd49446681e
Iconic Robbott <dev.robbott@gmail.com>
2025-02-27 16:13:33 +0100

    Implementing a Custom User Model in Django

--- advanced_features_and_security/LibraryProject/relationship_app/models.py
+++ advanced_features_and_security/LibraryProject/relationship_app/models.py
@@ -2,8 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django.contrib.auth.models import AbstractUser, BaseUserManager
-from django.db import models
 
 # Create your models here.
 
@@ -66,31 +64,3 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
-
-class CustomUserManager(BaseUserManager):
-    def create_user(self, username, email, password=None, date_of_birth=None, profile_photo=None):
-        if not email:
-            raise ValueError("Users must have an email address")
-        email = self.normalize_email(email)
-        user = self.model(username=username, email=email, date_of_birth=date_of_birth, profile_photo=profile_photo)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, username, email, password=None, date_of_birth=None, profile_photo=None):
-        user = self.create_user(username=username, email=email, password=password, date_of_birth=date_of_birth, profile_photo=profile_photo)
-        user.is_admin = True
-        user.is_staff = True
-        user.is_superuser = True
-        user.save(using=self._db)
-        return user
-
-class CustomUser(AbstractUser):
-    email = models.EmailField(unique=True)
-    date_of_birth = models.DateField(null=True, blank=True)
-    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
-
-    objects = CustomUserManager()
-
-    def __str__(self):
-        return self.username
